299e0a6b8012f9f86e82c440d910bc0e3cde2071
Gilson <gilsonsantos1998@gmail.comm>
2019-12-27 11:36:49 -0300

    adição do método getPost em post_services para fazermos a requisição

---
+++ json_in_flutter/lib/services/post_services.dart
@@ -0,0 +1,12 @@
+import 'package:json_in_flutter/model/post_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:io';
+
+String url = 'https://jsonplaceholder.typicode.com/posts';
+
+Future<Post> getPost() async{
+  final response = await http.get('$url/1');
+  return postFromJson(response.body);
+}
+
